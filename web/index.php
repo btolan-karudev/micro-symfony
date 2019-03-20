bca74426abbbcf7645e38cb59575bac04cc59f68
Beniamin Tolan <b.tolan@karudev-informatique.fr>
2019-03-20 22:05:49 +0100

    my firt mico framework

---
+++ web/index.php
@@ -0,0 +1,26 @@
+<?php
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+umask(0000);
+
+$loader = require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../AppKernel.php';
+
+// load the environmental variables
+//$dotenv = new Dotenv\Dotenv(__DIR__.'/../');
+//$dotenv->load();
+$env = 'dev';
+//$env = $_SERVER['SYMFONY_ENV'];
+$debug = true;
+//$debug = $_SERVER['SYMFONY_DEBUG'];
+
+if ($debug) {
+    Debug::enable();
+}
+
+$kernel = new AppKernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
